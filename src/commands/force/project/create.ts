0f9ae7aa726921bfb036353116d10e2eee2bbca8
Luis Campos <lcamposguajardo@salesforce.com>
2019-06-25 16:29:21 -0700

    Update gitignore, remove unnecessary files, add prettier

--- src/commands/force/project/create.ts
+++ src/commands/force/project/create.ts
@@ -5,9 +5,9 @@ import { AnyJson } from '@salesforce/ts-types';
 Messages.importMessagesDirectory(__dirname);
 
 export default class Project extends SfdxCommand {
-    public async run(): Promise<AnyJson> {
-        let outputString = 'This is the Project create';
-        console.log(outputString);
-        return {outputString};
+  public async run(): Promise<AnyJson> {
+    let outputString = 'This is the Project create';
+    console.log(outputString);
+    return { outputString };
   }
 }

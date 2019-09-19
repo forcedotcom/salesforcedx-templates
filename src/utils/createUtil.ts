7ac7cfa23f357158022b901226dac9ee34765410
Cade May <c.may@salesforce.com>
2019-09-19 10:44:34 -0700

    remove extra declarations

--- src/utils/createUtil.ts
+++ src/utils/createUtil.ts
@@ -4,9 +4,7 @@
  * Licensed under the BSD 3-Clause license.
  * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
-import { SfdxCommand } from '@salesforce/command';
 import { Messages } from '@salesforce/core';
-import { AnyJson } from '@salesforce/ts-types';
 import * as fs from 'fs';
 import * as path from 'path';
 // @ts-ignore

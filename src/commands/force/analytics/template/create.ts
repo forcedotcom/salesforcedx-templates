1fa66982e8a74c59d10dd6c9a31df949d09b2c7f
Reid <reid.elliott@salesforce.com>
2019-11-12 16:21:22 -0700

    updating message descriptions and help to new format

--- src/commands/force/analytics/template/create.ts
+++ src/commands/force/analytics/template/create.ts
@@ -9,29 +9,41 @@ import { Messages } from '@salesforce/core';
 import { AnyJson } from '@salesforce/ts-types';
 import * as path from 'path';
 import AnalyticsTemplateGenerator from '../../../../generators/analyticsTemplateGenerator';
-import { CreateUtil, TemplateCommand } from '../../../../utils';
+import { CreateUtil, MessageUtil, TemplateCommand } from '../../../../utils';
 
 Messages.importMessagesDirectory(__dirname);
-const messages = Messages.loadMessages('salesforcedx-templates', 'messages');
 export default class AnalyticsTemplate extends TemplateCommand {
   public static examples = [
     '$ sfdx force:analytics:template:create -n myTemplate -d outputdir'
   ];
 
-  public static description = messages.getMessage(
-    'CreateAnalyticsTemplateCommandDescription'
+  public static description = MessageUtil.buildDescription(
+    'AnalyticsTemplateDescription',
+    false
+  );
+
+  public static help = MessageUtil.buildHelpText(
+    AnalyticsTemplate.examples,
+    false
+  );
+  public static longDescription = MessageUtil.get(
+    'AnalyticsTemplateLongDescription'
   );
 
   protected static flagsConfig = {
     outputdir: flags.string({
       char: 'd',
-      description: messages.getMessage('outputdir'),
+      description: MessageUtil.get('OutputDirFlagDescription'),
+      longDescription: MessageUtil.get('OutputDirFlagLongDescription'),
       default: process.cwd()
     }),
     apiversion: flags.builtin(),
     templatename: flags.string({
       char: 'n',
-      description: messages.getMessage('AnalyticsTemplateName'),
+      description: MessageUtil.get('AnalyticsTemplateNameFlagDescription'),
+      longDescription: MessageUtil.get(
+        'AnalyticsTemplateNameFlagLongDescription'
+      ),
       required: true
     })
   };
@@ -42,7 +54,7 @@ export default class AnalyticsTemplate extends TemplateCommand {
     const fileparts = path.resolve(this.flags.outputdir).split(path.sep);
     // tslint:disable-next-line:no-unused-expression
     if (!fileparts.includes('waveTemplates')) {
-      throw new Error(messages.getMessage('MissingWaveTemplatesDir'));
+      throw new Error(MessageUtil.get('MissingWaveTemplatesDir'));
     }
 
     return this.runGenerator(AnalyticsTemplateGenerator);

289a8cddd3111783b813178968283fd2438351c2
Xiaoyi Chen <xchen@salesforce.com>
2020-07-28 17:54:58 -0700

    make plugins-templates module

---
+++ packages/templates/src/index.ts
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2020, salesforce.com, inc.
+ * All rights reserved.
+ * Licensed under the BSD 3-Clause license.
+ * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
+ */
+import AnalyticsTemplateGenerator from './generators/analyticsTemplateGenerator';
+import ApexClassGenerator from './generators/apexClassGenerator';
+import ApexTriggerGenerator from './generators/apexTriggerGenerator';
+import LightningAppGenerator from './generators/lightningAppGenerator';
+import LightningComponentGenerator from './generators/LightningComponentGenerator';
+import LightningEventGenerator from './generators/LightningEventGenerator';
+import LightningInterfaceGenerator from './generators/LightningInterfaceGenerator';
+import LightningTestGenerator from './generators/LightningTestGenerator';
+import ProjectGenerator from './generators/ProjectGenerator';
+import VisualforceComponentGenerator from './generators/VisualforceComponentGenerator';
+import VisualforcePageGenerator from './generators/VisualforcePageGenerator';
+
+export {
+  AnalyticsTemplateGenerator,
+  ApexClassGenerator,
+  ApexTriggerGenerator,
+  LightningAppGenerator,
+  LightningComponentGenerator,
+  LightningEventGenerator,
+  LightningInterfaceGenerator,
+  LightningTestGenerator,
+  ProjectGenerator,
+  VisualforceComponentGenerator,
+  VisualforcePageGenerator
+};

0e722d92ba5399bfa6cf2dc9cae6a1d3bed4727a
Xiaoyi Chen <xchen@salesforce.com>
2020-07-29 20:25:46 -0700

    remove yarn workspaces

--- packages/templates/src/utils/index.ts
+++ packages/templates/src/utils/index.ts
@@ -3,4 +3,3 @@ export { Log } from './logger';
 export { ForceGeneratorAdapter } from './adapter';
 export { Answers } from './types';
 export { MessageUtil } from './messageUtil';
-export { TemplateCommand } from './templateCommand';

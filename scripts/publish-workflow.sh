d6d4667a2a9c3e9df5d59fb3767539fe2eb4eb70
Shelby <shelby.holden@salesforce.com>
2020-10-01 16:42:27 -0600

    chore: add ability to provide custom version to publish

--- scripts/publish-workflow.sh
+++ scripts/publish-workflow.sh
@@ -5,11 +5,23 @@
 
 CircleCIToken=$1
 PublishType=$2
+PublishVersion=$3
+
+if [ ! -z "${PublishVersion}" ]; then
+  if [[ "${PublishVersion}" =~ ^[0-9]{2}\.[0-9]{1,2}\.[0-9]{1,2}$ ]]; then
+    echo "Publish version ${PublishVersion} is valid."
+  else
+    echo "Publish version ${PublishVersion} does not fit the format xx.yy.zz. Exiting."
+    exit 1
+  fi
+fi
+
 curl -v -u ${CircleCIToken}: -X POST --header "Content-Type: application/json" -d '{
   "branch": "main",
   "parameters": {
     "publish": true,
-    "publish-type": "'"${PublishType}"'"
+    "publish-type": "'"${PublishType}"'",
+    "publish-version": "'"${PublishVersion}"'"
   }
 }' https://circleci.com/api/v2/project/gh/forcedotcom/salesforcedx-templates/pipeline
 

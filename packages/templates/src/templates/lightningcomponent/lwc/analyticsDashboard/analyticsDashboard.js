8141254b701d31f0d3c549e9d14db91055365d08
Jeff Beeghly <jbeeghly@salesforce.com>
2022-05-26 12:22:08 -0700

    feat: added unit test to the LWC template

--- packages/templates/src/templates/lightningcomponent/lwc/analyticsDashboard/analyticsDashboard.js
+++ packages/templates/src/templates/lightningcomponent/lwc/analyticsDashboard/analyticsDashboard.js
@@ -1,6 +1,6 @@
 import { LightningElement, api } from 'lwc';
 
-export default class <%= className %> extends LightningElement {
+export default class <%= pascalCaseComponentName %> extends LightningElement {
   @api getState;
   @api setState;
   @api refresh;

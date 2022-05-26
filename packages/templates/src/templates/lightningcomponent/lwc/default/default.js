3c602793af4742f15bf08842b11bcc98ce6bd29c
Jeff Beeghly <jbeeghly@salesforce.com>
2022-05-26 09:11:44 -0700

    feat: added unit test to the LWC template

--- packages/templates/src/templates/lightningcomponent/lwc/default/default.js
+++ packages/templates/src/templates/lightningcomponent/lwc/default/default.js
@@ -1,3 +1,3 @@
 import { LightningElement } from 'lwc';
 
-export default class <%= className %> extends LightningElement {}
+export default class <%= pascalCaseComponentName %> extends LightningElement {}

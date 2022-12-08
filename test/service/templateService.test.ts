2d9845afdfb8b04273792b5caf787e20887128bd
Randi Wilson <randi274@gmail.com>
2022-12-08 18:40:42 -0500

    test: remove extra line and update tests for windows (#533)

--- test/service/templateService.test.ts
+++ test/service/templateService.test.ts
@@ -68,8 +68,7 @@ describe('TemplateService', () => {
 <ApexClass xmlns="http://soap.sforce.com/2006/04/metadata">
     <apiVersion>${apiVersion}</apiVersion>
     <status>Active</status>
-</ApexClass>
-`;
+</ApexClass>`;
       assert.file([expectedApexClassPath, expectedApexClassMetaPath]);
       assert.fileContent(expectedApexClassPath, expectedApexClassContent);
       assert.fileContent(
@@ -126,8 +125,7 @@ describe('TemplateService', () => {
 <ApexClass xmlns="http://soap.sforce.com/2006/04/metadata">
     <apiVersion>${apiVersion}</apiVersion>
     <status>Inactive</status>
-</ApexClass>
-`;
+</ApexClass>`;
       assert.file([expectedApexClassPath, expectedApexClassMetaPath]);
       assert.fileContent(expectedApexClassPath, expectedApexClassContent);
       assert.fileContent(

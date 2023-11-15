6c4a8c200375af3e8bff6e7372ea9270ef3cb165
peternhale <peternhale@users.noreply.github.com>
2023-11-15 11:33:03 -0700

    fix: upgrade min node engine to 18 (#570)
    * fix: upgrade to node 18
    
    @W-14196509@
    
    * chore: clean up lint issues due to rule changes
    
    * chore: update nvmrc to node 18

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -4,7 +4,6 @@
  * Licensed under the BSD 3-Clause license.
  * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
-'use strict';
 
 import * as util from 'util';
 
@@ -22,8 +21,6 @@ const statuses = [
 export class Log {
   private output = '';
   private cleanOutput: string[] = [];
-
-  // tslint:disable-next-line: no-any
   [index: string]: any;
 
   constructor() {

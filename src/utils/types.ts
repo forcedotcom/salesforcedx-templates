2771b0514ee29d23083c75b6605381f7fe3ebfea
Mike Donnalley <mdonnalley@salesforce.com>
2024-06-19 12:45:39 -0600

    chore: clean up

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -5,8 +5,6 @@
  * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
 
-export type Answers = Record<string, string>;
-
 export type CreateOutput = {
   outputDir: string;
   created: string[];

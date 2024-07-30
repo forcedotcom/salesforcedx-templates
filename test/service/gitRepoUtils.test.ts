f4dc06d8b9ce2f35d84c820474f3aad62020f1de
Mingxuan Zhang <132491513+mingxuanzhangsfdx@users.noreply.github.com>
2024-07-30 13:36:05 -0700

    feat: remove core from dependencies (#592)
    * feat: remove core from dependencies
    
    * chore: dir test
    
    * chore: apply suggestions

---
+++ test/service/gitRepoUtils.test.ts
@@ -0,0 +1,25 @@
+/*
+ * Copyright (c) 2024, salesforce.com, inc.
+ * All rights reserved.
+ * Licensed under the BSD 3-Clause license.
+ * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
+ */
+import { SinonStub, stub } from 'sinon';
+import { expect } from 'chai';
+import { DIR } from '../../src/service/gitRepoUtils';
+import * as os from 'os';
+import * as path from 'path';
+
+describe('DIR', () => {
+  let homedirStub: SinonStub;
+  beforeEach(() => {
+    homedirStub = stub(os, 'homedir');
+  });
+  it('should return DIR', () => {
+    const homedir = '/Users/johndoe';
+    homedirStub.returns(homedir);
+    const sfdxStateFolder = '.sfdx';
+    const dir = DIR();
+    expect(dir).to.eql(path.join(homedir, sfdxStateFolder));
+  });
+});

a75c49743fc56343545ec0d1f59187a6c9aa9470
mingxuanzhang <mingxuanzhang@salesforce.com>
2024-07-31 17:15:58 -0700

    feat: make templates bundle-able

--- scripts/build-templates.js
+++ scripts/build-templates.js
@@ -3,13 +3,15 @@ const fs = require('fs');
 const path = require('path');
 const shell = require('shelljs');
 
+// Determine the directory based on the 'esbuild' argument
+const isEsbuild = process.argv.includes('--esbuild');
 const currDir = process.cwd();
-const libDir = path.join(currDir, 'lib');
-const libTemplatesDir = path.join(libDir, 'templates');
+const outputDir = isEsbuild ? 'dist' : 'lib';
+const outputTemplatesDir = path.join(currDir, outputDir, 'templates');
 const srcTemplatesDir = path.join(currDir, 'src', 'templates');
 
-if (!fs.existsSync(libDir)) {
-  shell.mkdir(libDir);
+if (!fs.existsSync(outputDir)) {
+  shell.mkdir(outputDir);
 }
 
-shell.cp('-rf', srcTemplatesDir, libTemplatesDir);
+shell.cp('-rf', srcTemplatesDir, outputTemplatesDir);

54df39fa6e01b11e490d614d68c9a24428e127da
mingxuanzhang <mingxuanzhang@salesforce.com>
2024-08-01 13:06:12 -0700

    chore: revert esbuild-related artifacts

--- scripts/build-templates.js
+++ scripts/build-templates.js
@@ -1,17 +1,13 @@
-#!/usr/bin/env node
-const fs = require('fs');
 const path = require('path');
 const shell = require('shelljs');
 
-// Determine the directory based on the 'esbuild' argument
-const isEsbuild = process.argv.includes('--esbuild');
 const currDir = process.cwd();
-const outputDir = isEsbuild ? 'dist' : 'lib';
-const outputTemplatesDir = path.join(currDir, outputDir, 'templates');
+const libDir = path.join(currDir, 'lib');
+const libTemplatesDir = path.join(libDir, 'templates');
 const srcTemplatesDir = path.join(currDir, 'src', 'templates');
 
-if (!fs.existsSync(outputDir)) {
-  shell.mkdir(outputDir);
+if (!fs.existsSync(libDir)) {
+  shell.mkdir(libDir);
 }
 
-shell.cp('-rf', srcTemplatesDir, outputTemplatesDir);
+shell.cp('-rf', srcTemplatesDir, libTemplatesDir);

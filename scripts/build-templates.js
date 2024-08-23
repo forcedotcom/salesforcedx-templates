99ba16973b5e39a91a5bf7f20b96d52d4fed3846
Cristina Isabel Cañizales <cristina.canizales@salesforce.com>
2024-08-23 20:17:47 -0300

    chore: try modifying build-templates scripts

--- scripts/build-templates.js
+++ scripts/build-templates.js
@@ -7,9 +7,12 @@ const currDir = process.cwd();
 const libDir = path.join(currDir, 'lib');
 const libTemplatesDir = path.join(libDir, 'templates');
 const srcTemplatesDir = path.join(currDir, 'src', 'templates');
+const libUtilsDir = path.join(libDir, 'lib', 'utils');
+const srcUtilsDir = path.join(currDir, 'src', 'utils');
 
 if (!fs.existsSync(libDir)) {
   shell.mkdir(libDir);
 }
 
 shell.cp('-rf', srcTemplatesDir, libTemplatesDir);
+shell.cp('-rf', srcUtilsDir, libUtilsDir);

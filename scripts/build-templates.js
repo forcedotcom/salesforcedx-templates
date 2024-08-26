c107dceddcfd3e266119a8b5b1bb0366aab72ce7
Cristina Isabel Cañizales <cristina.canizales@salesforce.com>
2024-08-26 14:06:53 -0300

    chore: revert change in script

--- scripts/build-templates.js
+++ scripts/build-templates.js
@@ -7,12 +7,9 @@ const currDir = process.cwd();
 const libDir = path.join(currDir, 'lib');
 const libTemplatesDir = path.join(libDir, 'templates');
 const srcTemplatesDir = path.join(currDir, 'src', 'templates');
-const libUtilsDir = path.join(libDir, 'utils');
-const srcUtilsDir = path.join(currDir, 'src', 'utils');
 
 if (!fs.existsSync(libDir)) {
   shell.mkdir(libDir);
 }
 
 shell.cp('-rf', srcTemplatesDir, libTemplatesDir);
-shell.cp('-rf', srcUtilsDir, libUtilsDir);

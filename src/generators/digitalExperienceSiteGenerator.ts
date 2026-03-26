7bb02595ab8a907955a0b737c99746645b65dce2
Shubham Goyal <shubham.goyal@salesforce.com>
2026-03-26 16:13:40 +0530

    refactor: update crypto import to use node: prefix and clean up .gitignore @W-21523324@
    Changes:
    - Updated the crypto import in digitalExperienceSiteGenerator.ts to use the 'node:' prefix for better clarity and compatibility.
    - Removed the npm-packages-offline-cache entry from .gitignore as it is no longer needed.
    
    Benefits:
    - Aligns with best practices for module imports in Node.js.
    - Streamlines the .gitignore file for improved maintainability.
    
    Addresses review items #9 and #11 from Will's feedback

--- src/generators/digitalExperienceSiteGenerator.ts
+++ src/generators/digitalExperienceSiteGenerator.ts
@@ -4,7 +4,7 @@
  * Licensed under the BSD 3-Clause license.
  * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
-import * as crypto from 'crypto';
+import * as crypto from 'node:crypto';
 import * as path from 'path';
 import { CreateUtil } from '../utils';
 import { DigitalExperienceSiteOptions } from '../utils/types';
